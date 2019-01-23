270149a82540c6c672591f8645f22c74639b0768
Daniel <devidra87@gmail.com>
2019-01-22 18:33:09 -0800

    update searchDoctor Function to inlcude second parameter

--- src/main.js
+++ src/main.js
@@ -12,15 +12,15 @@ $(document).ready(function() {
       let name = $('#doctroName').val();
       $('#doctroName').val("");
       
-
     let doctor = new Doctor();  
-    let promise = doctor.searchDoctorbyName(name); 
+    let promise = doctor.searchDoctor(name, issue); 
 
       promise.then(function(response) {
         let body = JSON.parse(response);
+
         $('.results').text(`We Found the Following Doctors that Match Search Request:`);
-        for(let x = 0; x < (body.length); x++){
-            $('.showDoctors').append(`<li>City:${data.practices.profile.first_name}<br></li>`);
+        for(let x = 0; x < (body.data.length); x++){
+            $('.showDoctors').append(`<li>Name:${body.data[x].profile.first_name}<br></li>`);
             }
       }, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
