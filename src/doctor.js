a608819c5f0f0ee3226311f3b666927fb8947b16
Guest User <Guest@epicodus-25.local>
2019-02-06 16:32:48 -0800

    hide api key

--- src/doctor.js
+++ src/doctor.js
@@ -4,7 +4,7 @@ export class Doctor {
     searchDoctor(name, issue) {
         return new Promise(function (resolve, reject) {
             let request = new XMLHttpRequest();
-            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=de75973a60d3cc329b037711d5e5eadb
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}
             `;
             request.onload = function () {
                 if (this.status === 200) {
